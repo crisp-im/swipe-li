3ca2334c766ee12b04e0b67644cc386155520781
Baptiste Jamin <baptistejamin@gmail.com>
2016-06-05 00:23:43 +0200

    working

--- src/swipe-li.js
+++ src/swipe-li.js
@@ -8,137 +8,138 @@
 	Be sure to include optimize-animation.js to handle
 	requestAnimationFrame and cancel polyfill
 */
-
 angular.module('swipeLi')
-  .directive('swipeLi', ['hammerRemote', '$timeout', '$window', function (hammerRemote, $timeout, $window) {
-  	'use strict';
+  .directive('swipeLi', ['$timeout', '$window', '$swipe', function ($timeout, $window, $swipe) {
+    'use strict';
     return {
       restrict: 'A',
 
       link: function (scope, iElement, iAttrs) {
 
-				// On swipe complete
-				scope.onComplete = function (type) {
-					$timeout(function () {
-						scope.showPane(1, true);
-						scope.$apply(iAttrs[type]);
-					}, 300);
-				};
+        // On swipe complete
+        scope.onComplete = function (type) {
+          $timeout(function () {
+            scope.showPane(1, true);
+            scope.$apply(iAttrs[type]);
+          }, 300);
+        };
 
-      	// Basic 3 Pane Carousel
-				// animation between panes happens with css transitions
-			  var element = iElement.find('div');
-			  var container = iElement.find('ul');
-			  var panes = iElement.find('li');
-			  var pane_width = 0;
-			  var pane_count = 3;
-			  var current_pane = 0;
+        // Basic 3 Pane Carousel
+        // animation between panes happens with css transitions
 
-			  scope.init = function () {
-			    setPaneDimensions();
-			    // Display the content pane by default
-			    scope.showPane(1, false);
-			    // Handle resize and orientation change
-			    angular.element($window).on('load resize orientationchange', function () {
-			      setPaneDimensions();
-			    });
-			  };
+		    var MAX_VERTICAL_DISTANCE = 75;
+		    var MIN_HORIZONTAL_DISTANCE = 160;
+		    var MAX_VERTICAL_RATIO = 0.3;
 
-			  // Set the pane dimensions and scale the container
-			  function setPaneDimensions() {
-			    pane_width = element[0].offsetWidth;
-			    angular.forEach(panes, function (pane) {
-			      angular.element(pane).css({width : pane_width + 'px'});
-			    });
-			    angular.element(container).css({width : pane_width * pane_count + 'px'});
-			  }
+        var element = iElement.find('div');
+        var container = iElement.find('ul');
+        var panes = iElement.find('li');
+        var pane_width = 0;
+        var pane_count = 3;
+        var current_pane = 0;
 
-			  // Show pane by index
-			  scope.showPane = function (index, animate) {
-			    // between the bounds
-			    index = Math.max(0, Math.min(index, pane_count - 1));
-			    current_pane = index;
+        var startCoords = {
+        	x : 0,
+        	y : 0
+        }
 
-			    var offset = -((100 / pane_count) * current_pane);
-			    scope.setContainerOffset(offset, animate);
-			  };
+        var is_moving = false;
 
-			  scope.setContainerOffset = function (percent, animate) {
-			    container.removeClass('animate');
-			    if (animate) {
-			      container.addClass('animate');
-			    }
+        scope.init = function () {
+          setPaneDimensions();
+          // Display the content pane by default
+          scope.showPane(1, false);
+          // Handle resize and orientation change
+          angular.element($window).on('load resize orientationchange', function () {
+            setPaneDimensions();
+          });
+        };
 
-			    container.css('transform', 'translate3d(' + percent + '%,0,0) scale3d(1,1,1)');
-			  };
+        // Set the pane dimensions and scale the container
+        function setPaneDimensions() {
+          pane_width = element[0].offsetWidth;
+          angular.forEach(panes, function (pane) {
+            angular.element(pane).css({width : pane_width + 'px'});
+          });
+          angular.element(container).css({width : pane_width * pane_count + 'px'});
+        }
 
-			  // Handle hammertime touch events
-			  scope.handleHammer = function (ev) {
-			    // Disable browser scrolling
-			    ev.gesture.preventDefault();
-			    // Check to see if disabled or not
+        // Show pane by index
+        scope.showPane = function (index, animate) {
+          // between the bounds
+          index = Math.max(0, Math.min(index, pane_count - 1));
+          current_pane = index;
 
-			    switch (ev.type) {
-				    case 'dragright':
-				    case 'dragleft':
-				      // stick to the finger
-				      var pane_offset = -(100 / pane_count) * current_pane;
-				      var drag_offset = ((100 / pane_width) * ev.gesture.deltaX) / pane_count;
+          var offset = -((100 / pane_count) * current_pane);
+          scope.setContainerOffset(offset, animate);
+        };
 
-				      // slow down at the first and last pane
-				      if ((current_pane === 0 && ev.gesture.direction === 'right') ||
-				        (current_pane == pane_count - 1 && ev.gesture.direction == 'left')) {
-				        drag_offset *= 0.4;
-				      }
+        scope.setContainerOffset = function (percent, animate) {
+          container.removeClass('animate');
+          if (animate) {
+            container.addClass('animate');
+          }
 
-				      scope.setContainerOffset(drag_offset + pane_offset);
-				      break;
+          container.css('transform', 'translate3d(' + percent + '%,0,0) scale3d(1,1,1)');
+        };
 
-				    case 'swipeleft':
-				      scope.showPane(2, true);
-				      scope.onComplete('reject');
-				      ev.gesture.stopDetect();
-				      break;
+        scope.init();
 
-				    case 'swiperight':
-				      scope.showPane(0, true);
-				      scope.onComplete('accept');
-				      ev.gesture.stopDetect();
-				      break;
+        $swipe.bind(angular.element(element[0]), {
+          start : function(coords, event) {
+          	startCoords = coords;
+          	event.preventDefault();
+           	is_moving = true;
+          },
+          cancel : function(event) {
+          	//event.preventDefault();
+            is_moving = false;
+            scope.showPane(current_pane, true);
+          },
+          end : function(coords, event) {
+          	//event.preventDefault();
+          	is_moving = false;
+            scope.showPane(current_pane, true);
+          },
+          move : function(coords, event) {
 
-				    // More then 50% moved, navigate
-				    case 'release':
-				    	if (Math.abs(ev.gesture.deltaX) > pane_width / 2) {
-				        if (ev.gesture.direction == 'right') {
-				        	scope.showPane(0, true);
-				          scope.onComplete('accept');
-				        } else {
-				        	scope.showPane(2, true);
-				          scope.onComplete('reject');
-				        }
-				      } else {
-				        scope.showPane(current_pane, true);
-				      }
-				      break;
-			    }
-			  };
+          	event.preventDefault();
 
-				// Register a new hammer instance
-				scope.id = hammerRemote.register(
-					element[0],
-					{ drag_lock_to_axis: true });
+          	var deltaY = Math.abs(coords.y - startCoords.y);
+          	var deltaX = (coords.x - startCoords.x);
 
-				// Init the carousel
-				hammerRemote.getHammer(scope.id)
-					.then(function (hammertime) {
-						hammertime.on('release dragleft dragright swipeleft swiperight', scope.handleHammer);
-						scope.init();
-					});
+          	console.log(deltaX);
 
-				// Handle destroy for hammer
-        scope.$on('$destroy', function () {
-          hammerRemote.unregister(scope.id);
-        });
+ 						if (is_moving && deltaY < MAX_VERTICAL_DISTANCE
+ 							&& deltaY / deltaX < MAX_VERTICAL_RATIO) {
+
+
+ 							if ((Math.abs(deltaX) > MIN_HORIZONTAL_DISTANCE)) {
+ 								if (deltaX > 0) {
+ 									scope.showPane(0, true);
+              		scope.onComplete('accept');
+ 								} else {
+ 									scope.showPane(2, true);
+              		scope.onComplete('reject');
+ 								}
+ 							} else {
+		          	var pane_offset = -(100 / pane_count) * current_pane;
+		          	var drag_offset = ((100 / pane_width) * deltaX) / pane_count;
+
+		          	if (current_pane === 0 && deltaX > 0 ||
+		          		(deltaX < 0 && current_pane == pane_count - 1)) {
+		              drag_offset *= 0.4;
+		            }
+
+		         		scope.setContainerOffset(drag_offset + pane_offset);
+	         		}
+         		}
+         		else {
+         			is_moving = false;
+         		}
+
+          }
+        }, ["touch", "mouse"]);
       }
-    };
+    }
   }]);
